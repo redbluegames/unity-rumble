73a9e6216f0379930bfec955a7ae62a999ee3adc
Edward Rowe <edward.l.rowe@gmail.com>
2020-12-23 23:21:32 -0500

    Add "failing" happy path tests for RumbleSource

---
+++ Assets/Red Blue Games/Rumble/Tests/RumbleSourceTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace RedBlueGames.Rumble.Tests
+{
+    public class RumbleSourceTests
+    {
+        [Test]
+        public void Initialize_Default_IsAlive()
+        {
+            
+        }
+        
+        [Test]
+        public void Initialize_DeadSource_IsAlive()
+        {
+            
+        }
+        
+        [Test]
+        public void IsDead_TimeRemains_False()
+        {
+            
+        }
+        
+        [Test]
+        public void IsDead_ZeroTime_True()
+        {
+            
+        }
+        
+        [Test]
+        public void Tick_TimeRemains_StillAlive()
+        {
+            
+        }
+        
+        [Test]
+        public void Tick_TimeExpires_Dies()
+        {
+            
+        }
+        
+        [Test]
+        public void Tick_DeadSource_StillDead()
+        {
+            
+        }
+        
+        [Test]
+        public void EvaluateRumble_ListenerAtCenter_FullStrength()
+        {
+            
+        }
+        
+        [Test]
+        public void EvaluateRumble_ListenerOutOfRange_ZeroRumble()
+        {
+            
+        }
+        
+        [Test]
+        public void EvaluateRumble_ListenerHalfway_HalfRumble()
+        {
+            
+        }
+    }
+}
