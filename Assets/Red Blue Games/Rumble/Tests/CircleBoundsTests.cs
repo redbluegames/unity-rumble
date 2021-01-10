8f1f71767ad397ad6de1310209e05628a65aa8db
Edward Rowe <edward.l.rowe@gmail.com>
2021-01-09 22:24:58 -0500

    WIP Add tests for CircleBounds

---
+++ Assets/Red Blue Games/Rumble/Tests/CircleBoundsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace RedBlueGames.Rumble.Tests
+{
+    public class CircleBoundsTests
+    {
+        [Test]
+        public void GetPercentFromCenter_AtCenter_IsZero()
+        {
+            var bounds = new CircleBounds(Vector3.one, 10.0f);
+            var percentFromCenter = bounds.GetPercentFromCenter(Vector3.one);
+            Assert.That(percentFromCenter, Is.EqualTo(0.0f));
+        }
+        
+        [Test]
+        public void GetPercentFromCenter_AtRadius_IsOne()
+        {
+            var radius = 10.0f;
+            var startPosition = Vector3.zero;
+            var bounds = new CircleBounds(startPosition, radius);
+            var percentFromCenter = bounds.GetPercentFromCenter(
+                startPosition + new Vector3(1.0f, 0.0f, 0.0f) * radius);
+            Assert.That(percentFromCenter, Is.EqualTo(1.0f));
+        }
+        
+        [Test]
+        public void GetPercentFromCenter_OutOfBounds_IsGreaterThanOne()
+        {
+            var radius = 10.0f;
+            var startPosition = Vector3.zero;
+            var bounds = new CircleBounds(startPosition, radius);
+            var percentFromCenter = bounds.GetPercentFromCenter(
+                startPosition + new Vector3(1.0f, 0.0f, 0.0f) * radius * 2.0f);
+            Assert.That(percentFromCenter, Is.EqualTo(2.0f));
+        }
+    }
+}
