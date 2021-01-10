0f8903ec92e5ad6a184f2bcaa4c6ba11954ba271
Edward Rowe <edward.l.rowe@gmail.com>
2021-01-09 22:46:07 -0500

    Add Curve Distance and Time Modulator tests

---
+++ Assets/Red Blue Games/Rumble/Tests/CurveDistanceModulatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace RedBlueGames.Rumble.Tests
+{
+    public class CurveDistanceModulatorTests
+    {
+        private readonly AnimationCurve linearOneToZero = AnimationCurve.Linear(0.0f, 1.0f, 1.0f, 0.0f);
+        
+        [Test]
+        public void CalculateFalloff_LinearOneToZeroAtCenterPercent_IsOne()
+        {
+            var modulator = new CurveDistanceModulator(linearOneToZero);
+            var intensity = modulator.CalculateFalloff(RumbleIntensity.One, 0.0f);
+            Assert.That(intensity, Is.EqualTo(RumbleIntensity.One));
+        }
+        
+        [Test]
+        public void CalculateFalloff_LinearOneToZeroAtHalfPercent_IsHalf()
+        {
+            var modulator = new CurveDistanceModulator(linearOneToZero);
+            var intensity = modulator.CalculateFalloff(RumbleIntensity.One, 0.5f);
+            Assert.That(intensity, Is.EqualTo(
+                new RumbleIntensity(
+                    ForceFeedbackIntensities.One * 0.5f, 
+                    ScreenShakeIntensities.One * 0.5f)));
+        }
+        
+        [Test]
+        public void CalculateFalloff_LinearOneToZeroAtFullPercent_IsZero()
+        {
+            var modulator = new CurveDistanceModulator(linearOneToZero);
+            var intensity = modulator.CalculateFalloff(RumbleIntensity.One, 1.0f);
+            Assert.That(intensity, Is.EqualTo(
+                new RumbleIntensity(
+                    ForceFeedbackIntensities.One * 0.0f, 
+                    ScreenShakeIntensities.One * 0.0f)));
+        }
+    }
+}
